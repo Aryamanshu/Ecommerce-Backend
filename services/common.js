f82e48a573529cf368017f28493ae16b59962e53
Aryamanshu Mishra <93377648+Aryamanshu@users.noreply.github.com>
2024-09-24 20:00:29 +0530

    multiple changes

--- services/common.js
+++ services/common.js
@@ -6,4 +6,14 @@ exports.isAuth = (req, res, done) => {
 
 exports.sanitizeUser = (user)=>{
     return {id:user.id, role:user.role}
-}
+};
+
+exports.cookieExtractor = function (req) {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies['jwt'];
+  }
+  //TODO : this is temporary token for testing without cookie //koi bhi order krega par register isi token k user par hoga
+  token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2ZjJjMTQ2ZTgxOWVjNzMxN2UxMjY1MyIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzI3MTg1MjIyfQ.ApUCYGvRnAOytI9s1t7OElbSs2O8sRbaMrqcb_H8jqA"
+  return token;
+};
