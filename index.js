75dde37c768da7ee422254f99651ffec237f550d
Aryamanshu Mishra <93377648+Aryamanshu@users.noreply.github.com>
2024-09-24 20:00:16 +0530

    multiple changes

--- index.js
+++ index.js
@@ -1,81 +1,86 @@
-const express = require('express');
+const express = require("express");
 const server = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
-const session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const crypto = require('crypto');
-const jwt = require('jsonwebtoken');
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const { createProduct } = require('./controller/Product');
-const productsRouter = require('./routes/Products');
-const categoriesRouter = require('./routes/Category');
-const brandsRouter = require('./routes/Brands');
-const usersRouter = require('./routes/Users');
-const authRouter = require('./routes/Auth');
-const cartRouter = require('./routes/Cart');
-const ordersRouter = require('./routes/Order');
-const { User } = require('./model/User');
-const { isAuth, sanitizeUser } = require('./services/common');
+const mongoose = require("mongoose");
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const cookieParser = require("cookie-parser");
+const { createProduct } = require("./controller/Product");
+const productsRouter = require("./routes/Products");
+const categoriesRouter = require("./routes/Category");
+const brandsRouter = require("./routes/Brands");
+const usersRouter = require("./routes/Users");
+const authRouter = require("./routes/Auth");
+const cartRouter = require("./routes/Cart");
+const ordersRouter = require("./routes/Order");
+const { User } = require("./model/User");
+const { isAuth, sanitizeUser, cookieExtractor } = require("./services/common");
 
-const SECRET_KEY = 'SECRET_KEY';
+const SECRET_KEY = "SECRET_KEY";
 // JWT options
 const opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.jwtFromRequest = cookieExtractor;
 opts.secretOrKey = SECRET_KEY; // TODO: should not be in code;
 
 //middlewares
 
+server.use(express.static("build"));
+server.use(cookieParser());
 server.use(
   session({
-    secret: 'keyboard cat',
+    secret: "keyboard cat",
     resave: false, // don't save session if unmodified
     saveUninitialized: false, // don't create session until something stored
   })
 );
-server.use(passport.authenticate('session'));
+server.use(passport.authenticate("session"));
 server.use(
   cors({
-    exposedHeaders: ['X-Total-Count'],
+    exposedHeaders: ["X-Total-Count"],
   })
 );
 server.use(express.json()); // to parse req.body
-server.use('/products', isAuth(), productsRouter.router);
+server.use("/products", isAuth(), productsRouter.router);
 // we can also use JWT token for client-only auth
-server.use('/categories', isAuth(), categoriesRouter.router);
-server.use('/brands', isAuth(), brandsRouter.router);
-server.use('/users', isAuth(), usersRouter.router);
-server.use('/auth', authRouter.router);
-server.use('/cart', isAuth(), cartRouter.router);
-server.use('/orders', isAuth(), ordersRouter.router);
+server.use("/categories", isAuth(), categoriesRouter.router);
+server.use("/brands", isAuth(), brandsRouter.router);
+server.use("/users", isAuth(), usersRouter.router);
+server.use("/auth", authRouter.router);
+server.use("/cart", isAuth(), cartRouter.router);
+server.use("/orders", isAuth(), ordersRouter.router);
 
 // Passport Strategies
 passport.use(
-  'local',
-  new LocalStrategy(
-    {usernameField: 'email'},
-    async function (email, password, done) {
+  "local",
+  new LocalStrategy({ usernameField: "email" }, async function (
+    email,
+    password,
+    done
+  ) {
     // by default passport uses username
     try {
       const user = await User.findOne({ email: email });
       console.log(email, password, user);
       if (!user) {
-        return done(null, false, { message: 'invalid credentials' }); // for safety
+        return done(null, false, { message: "invalid credentials" }); // for safety
       }
       crypto.pbkdf2(
         password,
         user.salt,
         310000,
         32,
-        'sha256',
+        "sha256",
         async function (err, hashedPassword) {
           if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
-            return done(null, false, { message: 'invalid credentials' });
+            return done(null, false, { message: "invalid credentials" });
           }
           const token = jwt.sign(sanitizeUser(user), SECRET_KEY);
-          done(null, token); // this lines sends to serializer
+          done(null, { id: user.id, role: user.role }); // this lines sends to serializer
         }
       );
     } catch (err) {
@@ -85,7 +90,7 @@ passport.use(
 );
 
 passport.use(
-  'jwt',
+  "jwt",
   new JwtStrategy(opts, async function (jwt_payload, done) {
     console.log({ jwt_payload });
     try {
@@ -103,7 +108,7 @@ passport.use(
 
 // this creates session variable req.user on being called from callbacks
 passport.serializeUser(function (user, cb) {
-  console.log('serialize', user);
+  console.log("serialize", user);
   process.nextTick(function () {
     return cb(null, { id: user.id, role: user.role });
   });
@@ -112,7 +117,7 @@ passport.serializeUser(function (user, cb) {
 // this changes session variable req.user when called from authorized request
 
 passport.deserializeUser(function (user, cb) {
-  console.log('de-serialize', user);
+  console.log("de-serialize", user);
   process.nextTick(function () {
     return cb(null, user);
   });
@@ -121,10 +126,10 @@ passport.deserializeUser(function (user, cb) {
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce');
-  console.log('database connected');
+  await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
+  console.log("database connected");
 }
 
 server.listen(8080, () => {
-  console.log('server started');
+  console.log("server started");
 });
